fe0c71317921bcfb3e2c34253cd533f75d4da3f3
ChristianEspinoza <christianalfonsoespinoza@gmail.com>
2019-06-27 09:23:59 -0400

    initialize firebase react project

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Firebase extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default Firebase;
---
+++ src/components/SignOut/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class SignOut extends React.Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+
+    )
+  }
+}
+
+export default SignOut;
